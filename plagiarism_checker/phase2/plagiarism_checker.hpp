1bbb5a5a45f6a1329f2c00eb396ff4cf09673829
Abhilasha Sharma Suman <147299889+Io1207@users.noreply.github.com>
2024-11-25 00:02:21 +0530

    Update plagiarism_checker.hpp

--- plagiarism_checker/phase2/plagiarism_checker.hpp
+++ plagiarism_checker/phase2/plagiarism_checker.hpp
@@ -1,5 +1,6 @@
 #include "structures.hpp"
 // -----------------------------------------------------------------------------
+// Standard library includes
 #include <vector>
 #include <thread>
 #include <mutex>
@@ -11,11 +12,8 @@
 // DO NOT add "using namespace std;" or include any other files/libraries.
 // Also DO NOT add the include "bits/stdc++.h"
 
-// OPTIONAL: Add your helper functions and classes here
-
 const int base = 257;
 const int mod = 1e9 + 7;
-
 const int num_threads = 3;
 
 struct tokenized_submission_t : public submission_t
@@ -25,52 +23,41 @@ struct tokenized_submission_t : public submission_t
     std::vector<long long> hashVec;
     std::unordered_set<long long> hashSet;
     bool has_been_flagged; // To ensure we don't flag the same submission multiple times
-    bool is_new_submission; // We are not flagging pre-existing submissions that are given in the constructor
-    std::mutex flag_mutex; // To ensure thread safety
+    bool is_new_submission; // To differentiate new and pre-existing submissions
+    std::mutex flag_mutex;  // Mutex for thread safety
     int patch_small;
     int patch_long;
 
     tokenized_submission_t(
-        std::shared_ptr<submission_t> submission, const std::vector<int> &tokens, 
-        const std::chrono::time_point<std::chrono::steady_clock> &timestamp, 
+        std::shared_ptr<submission_t> submission, const std::vector<int> &tokens,
+        const std::chrono::time_point<std::chrono::steady_clock> &timestamp,
         const std::vector<long long> &hashVec, const std::unordered_set<long long> &hashSet,
-        const bool &is_new_submission
-    );
+        const bool &is_new_submission);
     void flag_plagiarism();
 };
 
 class plagiarism_checker_t
 {
-    // You should NOT modify the public interface of this class.
 public:
     plagiarism_checker_t(void);
-    plagiarism_checker_t(std::vector<std::shared_ptr<submission_t>>
-                             __submissions);
+    plagiarism_checker_t(std::vector<std::shared_ptr<submission_t>> __submissions);
     ~plagiarism_checker_t(void);
     void add_submission(std::shared_ptr<submission_t> __submission);
 
 protected:
-    // TODO: Add members and function signatures here
-
     void process_submission(std::shared_ptr<submission_t> submission, std::chrono::time_point<std::chrono::steady_clock> timestamp);
     std::shared_ptr<tokenized_submission_t> get_tokenized_submission(
         const std::shared_ptr<submission_t> &submission, const std::chrono::time_point<std::chrono::steady_clock> &timestamp,
-        const bool &is_new_submission
-    );
+        const bool &is_new_submission);
     void tokenize_hash_chunk(const std::vector<std::shared_ptr<submission_t>> &submissions, const int &start, const int &end, const auto &timestamp);
-
-    // void check_exact_match(const int &i, const int &j);
-
-    // Parallel processing
-    std::vector<std::thread> threads;
-
-    // Number of submissions
-    int n;
+    void plag_check_chunk(const int &start, const int &end);
 
     std::vector<std::shared_ptr<tokenized_submission_t>> tokenized_submissions;
 
-    // To ensure thread safety
+    // Thread management
+    std::vector<std::thread> threads;
     std::mutex sub_mutex;
 
-    // End TODO
+    // Number of submissions
+    int n;
 };
